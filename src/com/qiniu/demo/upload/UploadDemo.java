601953186830375ea1d676fe591e9a67d6a6de8c
liangchao071125 <liangchao071125@gmail.com>
2015-09-25 13:55:55 +0800

    simple qiniu-java-demo

---
+++ src/com/qiniu/demo/upload/UploadDemo.java
@@ -0,0 +1,15 @@
+package com.qiniu.demo.upload;
+
+import com.qiniu.util.Auth;
+
+public class UploadDemo {
+	
+	public String accessKey = "";
+	
+	public String secretKey = "";
+	
+	public Auth auth = Auth.create(accessKey, secretKey);
+	
+	
+
+}
