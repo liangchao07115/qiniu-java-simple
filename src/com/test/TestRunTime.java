c21b3c9858f63cd7df75224b644490f5e18f657b
liangchao071125 <liangchao071125@gmail.com>
2015-10-11 20:36:26 +0800

    RunTime

---
+++ src/com/test/TestRunTime.java
@@ -0,0 +1,40 @@
+package com.test;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.junit.Test;
+
+public class TestRunTime {
+
+	@Test
+	public void test() throws InterruptedException{
+		Runtime runTime = Runtime.getRuntime(); //get Runtime object
+		String[] cmd = new String[2];
+		cmd[0] = "cmd.exe";
+		cmd[1] = "start ping www.baidu.com";
+		try {
+			Process p = runTime.exec("ls -l");
+			
+			BufferedInputStream in = new BufferedInputStream(p.getInputStream());  
+            BufferedReader inBr = new BufferedReader(new InputStreamReader(in));  
+            String lineStr;             
+            while ((lineStr = inBr.readLine()) != null)  
+                System.out.println(lineStr);// 打印输出信息  
+            if (p.waitFor() != 0) {  
+                if (p.exitValue() == 1)//p.exitValue()==0表示正常结束，1：非正常结束  
+                    System.err.println("命令执行失败!");  
+            }  
+            inBr.close();  
+            in.close();  
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
