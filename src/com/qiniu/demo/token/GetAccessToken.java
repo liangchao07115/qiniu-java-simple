7eced3df69aac572ea4ea9f5082af42190a1b690
liangchao071125 <liangchao071125@gmail.com>
2015-10-20 12:51:56 +0800

    token

--- src/com/qiniu/demo/token/GetAccessToken.java
+++ src/com/qiniu/demo/token/GetAccessToken.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import org.junit.Test;
 
-import com.qiniu.common.Config;
+import com.qiniu.config.Config;
 import com.qiniu.util.Auth;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -12,11 +12,7 @@ import com.squareup.okhttp.Response;
 
 public class GetAccessToken {	
 	public String signingStr = "/drop/diaobotest\n";
-	
-	public String ak = "ak";
-	
-	public String sk = "sk";
-	
+		
 	public Auth auth = Auth.create(Config.ak, Config.sk);	
 	
 	//  获取管理凭证
